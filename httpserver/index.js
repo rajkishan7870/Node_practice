4ff2a553b1bc73d84d3fbe6d2b7391d29bea9505
kishan kumar <rajkishan7870@gmail.com>
2023-07-29 00:32:06 +0530

    routing

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req,res)=>{
-    res.end("hello from other side");
+    if(req.url == '/'){
+        res.end("hello from home side");
+    }
+    else if(req.url == '/contact'){
+        res.end("hello from contact side");
+    }
+    else if(req.url == '/about'){
+        res.end("hello from about side");
+    }
+    else{
+        res.writeHead(404,{"content-type" : "text/html"});
+        res.end("<h1>404 error page!!</h1>")
+    }
+    
 });
 
 server.listen(8000,()=>{
