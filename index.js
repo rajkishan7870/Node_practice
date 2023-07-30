d7dee189f64d71c4f66ec33d2f9b711a02c0b07e
kishan kumar <rajkishan7870@gmail.com>
2023-07-30 15:19:41 +0530

    dynamic content using template engine hbs in express

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const path = require('path');
 const app = express();
 
 // console.log(path.join(__dirname,"../public"))
-const staticpath = path.join(__dirname,"../public");
+const staticpath = path.join(__dirname,"../views");
+
 //built in MiddleWare:
-app.use(express.static(staticpath))
+// app.use(express.static(staticpath))
 
-app.get("/", (req,res)=>{
-    res.send("hello from express home page");
-})
+app.set("views",staticpath)
+app.set("view engine", "hbs");
 
-app.get("/about", (req,res)=>{
-    res.send("hello from express about page")
-})
+app.get("/",(req,res)=>{
+    res.render("index", {
+        name : "Kishan",
+    });
+});
+
+// app.get("/", (req,res)=>{
+//     res.send("hello from express home page");
+// })
+
+// app.get("/about", (req,res)=>{
+//     res.send("hello from express about page")
+// })
 
 app.listen(3000, ()=>{
     console.log("listening port")
