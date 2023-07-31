3ff6bc386daf206650d25be71067ff820cc75447
kishan kumar <rajkishan7870@gmail.com>
2023-07-31 17:15:02 +0530

    read data from mongoose

--- mongoosedata/app.js
+++ mongoosedata/app.js
@@ -65,4 +65,15 @@ const createDocument = async () =>{
     }
 }
 
-createDocument();
+//createDocument();
+
+
+const getDocument = async ()=>{
+    const result = await Playlist.find();
+    console.log(result);
+}
+
+getDocument();
+
+
+
