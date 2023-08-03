d9b37fe3360dc641546b5138b1e10b6182560283
kishan kumar <rajkishan7870@gmail.com>
2023-08-03 19:20:16 +0530

    curd with express, mongoose and postman

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -7,15 +7,58 @@ const Student = require("./models/students")
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-app.post("/students", (req,res)=>{
+app.post("/students", async (req,res)=>{
     //// console.log(req.body);
-    const user = new Student(req.body);
-    user.save().then(()=>{
-        res.status(201).send(user);
-    }).catch((err)=>{
-        res.status(404).send(err);
-    })
-    
+    try{
+        const user =  new Student(req.body);
+        const studentData = await user.save();
+        res.status(201).send(studentData)
+    }catch(err){
+        res.status(404).send("getting error")
+    }   
+})
+//get data from mongo.....
+app.get("/students", async(req,res)=>{
+    try{
+        const studentData = await Student.find();
+        res.send(studentData);
+    }catch(err){
+        res.send(err)
+    }
+})
+
+//get indivisual data by using id .......
+
+app.get("/students/:id", async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const studentData = await Student.findById({_id : _id});
+        res.send(studentData);
+    }catch(err){
+        res.send(err);
+    }
+})
+
+app.patch("/students/:id", async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const studentData = await Student.findByIdAndUpdate({_id : _id},req.body, {
+            new : true
+        });
+        res.send(studentData);
+    }catch(err){
+        res.send(err);
+    }
+})
+
+app.delete("/students/:id", async(req,res)=>{
+    try{
+        const _id = req.params.id;
+        const studentData = await Student.findByIdAndDelete(_id);
+        res.send(studentData);
+    }catch(err){
+        res.send(err);
+    }
 })
 
 app.listen(port,()=>{
